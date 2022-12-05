3fc25fc8c03843e4f1f6e5573813089fe6396476
Etha <76168237+etharion29@users.noreply.github.com>
2022-12-05 22:43:16 +0100

    Welcome page is finished

--- src/components/FooterHome/FooterHome.jsx
+++ src/components/FooterHome/FooterHome.jsx
@@ -1,22 +1,28 @@
 import './footer_home.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faVihara } from '@fortawesome/free-solid-svg-icons';
+import { faMedal, faSeedling, faUsers, faVihara } from '@fortawesome/free-solid-svg-icons';
 
 const FooterHome = () => {
     return (
         <div className='footer'>
             <div className='footer_principes'>
                 <div className='footer_principe'>      
-                    <FontAwesomeIcon icon={faVihara} className='footer_icon_vihara' size="2xl" color="#0AAE11"/>
+                    <FontAwesomeIcon icon={faVihara} size="2xl" color="#0AAE11"/>
                     <span className='footer_principe_1'>Respect</span>
+                </div>
+                <div className='footer_principe'>      
+                    <FontAwesomeIcon icon={faMedal} size="2xl" color="#AE0A14"/>
+                    <span className='footer_principe_2'>Combativité</span>
                 </div>
             </div>
             <div className='footer_principes'>
-            <   div className='footer_principe'>
-                    
+            <div className='footer_principe'>      
+                    <FontAwesomeIcon icon={faSeedling} size="2xl" color="#0BB6AB"/>
+                    <span className='footer_principe_3'>Humilité</span>
                 </div>
-                <div className='footer_principe'>
-                    
+                <div className='footer_principe'>      
+                    <FontAwesomeIcon icon={faUsers} size="2xl" color="#8B0ED7"/>
+                    <span className='footer_principe_4'>Partage</span>
                 </div>
             </div>
         </div>
